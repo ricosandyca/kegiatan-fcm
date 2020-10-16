45e9f228878feae35a19b6c4c0d58423ce7bcb64
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-10-16 15:32:57 +0700

    Add custom file upload hook

---
+++ src/hooks/useFileUpload.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import firebase from 'firebase/app'
+
+export default function () {
+  const [ref, setRef] = useState<firebase.storage.Reference>()
+
+  const [percentage, setPercentage] = useState(0)
+  const [isComplete, setIsComplete] = useState(false)
+  const [error, setError] = useState<Error>()
+  const [downloadURL, setDownloadURL] = useState<string>()
+
+  useEffect(() => {
+    if (isComplete)
+      ref?.getDownloadURL()
+        .then(downloadURL => setDownloadURL(downloadURL))
+        .catch(() => setDownloadURL(undefined))
+  }, [ref, isComplete])
+
+  const upload = (prefix: string, file: File) => {
+    firebase
+      .storage()
+      .ref(`${prefix}/${file.name}`)
+      .put(file)
+      .on(
+        'state_changed',
+        function progress (progress) {
+          setRef(progress.ref)
+          setPercentage((progress.bytesTransferred / file.size) * 100)
+        },
+        function error (err) {
+          setError(err)
+        },
+        function complete () {
+          setIsComplete(true)
+        }
+      )
+  }
+
+  return { percentage, isComplete, error, downloadURL, upload }
+}
