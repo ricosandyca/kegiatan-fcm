f0e22d42749eec807bc573c18fe25407d6c3b4fa
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-10-16 15:33:37 +0700

    Implement the file upload hook

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -1,50 +1,35 @@
 import React, { useState } from 'react'
-import firebase from 'firebase/app'
 
 import getCurrentUser from '../services/auth/current-user'
+import useUploadFile from '../hooks/useFileUpload'
 
 export default function () {
-  const storage = firebase.storage()
-  const [file, setFile] = useState<File|undefined>(undefined)
-  const [percentage, setPercentage] = useState(0)
+  const [file, setFile] = useState<File>()
+  const { percentage, isComplete, error, downloadURL, upload } = useUploadFile()
 
   function onChange (e: React.ChangeEvent<HTMLInputElement>) {
-    e.preventDefault()
-    const files = e.target.files
-    if (files) setFile(files[0])
+    if (e.target.files && e.target.files.length >= 1) {
+      setFile(e.target.files[0])
+    }
   }
 
-  function onSubmit (e: React.FormEvent) {
+  function onSubmit (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    if (!file) return alert('Empty')
-    const userId = getCurrentUser()?.uid || ''
-    const storageRef = storage.ref(`${userId}/${file.name}`)
-    storageRef
-      .put(file)
-      .on(
-        'state_changed',
-        function progress (snapshot) {
-          const currPercentage = (snapshot.bytesTransferred / file.size) * 100
-          setPercentage(currPercentage)
-        },
-        function error (err) {
-          alert(err.message)
-        },
-        function completed () {
-          alert('Uploaded')
-        }
-      )
+    const userId = getCurrentUser()?.uid
+    if (file && userId) upload(userId, file)
   }
 
   return (
     <div>
-      <br /><br /><br /><br />
+      <br/><br/><br/><br/>
       <form onSubmit={onSubmit}>
         <input type='file' onChange={onChange}/>
         <input type='submit' value='Upload'/>
       </form>
-      <div>Percentage: {percentage}%</div>
-      <div>Filename: {file?.name || ''}</div>
+      <div><b>Percentage: </b>{percentage}%</div>
+      <div><b>Download URL: </b>{downloadURL}</div>
+      <div><b>Status: </b>{isComplete ? 'Completed' : ''}</div>
+      <div><b>Error: </b>{error?.message}</div>
     </div>
   )
 }
