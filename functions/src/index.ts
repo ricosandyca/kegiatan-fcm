47ae2fe2fde14a458341108f5e9a6368f72fb4e9
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-10-19 23:18:35 +0700

    Add firebase functions to become an admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+export const becomeAdmin = functions.https.onCall(async (data, context) => {
+  // log request data
+  functions.logger.log('becomeAdmin function invoked...')
+  functions.logger.log('data', data)
+
+  if (!context.auth) throw new Error('Not authorized')
+  await admin
+    .auth()
+    .setCustomUserClaims(context.auth.uid, { admin: true })
+  return { message: 'Successfully become admin' }
+})
